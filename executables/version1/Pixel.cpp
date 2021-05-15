5340561cd9a57173f8fa4c194123092e4eeb9818
Jacob <70961645+Jacob-Hampton@users.noreply.github.com>
2021-05-14 20:44:35 -0400

    Update Pixel.cpp

--- executables/version1/Pixel.cpp
+++ executables/version1/Pixel.cpp
@@ -1,10 +1,19 @@
 #include "Pixel.h"
 
-Pixel createPixel(char r,char g, char b, char status){
+Pixel create_pixel(unsigned char r,unsigned char g,unsigned char b,unsigned char status){
     Pixel pix;
     pix.r = r;
     pix.g = g;
     pix.b = b;
     pix.status = status;
+    return pix;
+}
+
+Pixel average_pixel(Pixel pix1, Pixel pix2){
+    Pixel pix;
+    pix.r = (pix1.r + pix2.r)/2;
+    pix.g = (pix1.g + pix2.g)/2;
+    pix.b = (pix1.b + pix2.b)/2;
+    pix.status = ON;
     return pix;
 }
